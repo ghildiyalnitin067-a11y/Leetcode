486e755f84350d54b2baed8e263d6e9647fd90bd
Nitin Ghildiyal <ghildiyalnitin067@gmail.com>
2026-07-14 21:13:19 +0530

    Time: 1082 ms (5.06%), Space: 57.2 MB (94.49%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,69 +1,18 @@
 class Solution {
 public:
+    int reversePairs(vector<int>& a) {
+        int ans = 0;  
+        int n = a.size(); 
+        vector<long long> v;  
+       
+        for (int i = n - 1; i >= 0; i--) {
+          
+            ans += lower_bound(v.begin(), v.end(), a[i]) - v.begin();
 
-    int merge(vector<int>& nums, int low, int mid, int high) {
-
-        int count = 0;
-        int right = mid + 1;
-
-      
-        for (int left = low; left <= mid; left++) {
-            while (right <= high && nums[left] > 2LL * nums[right]) {
-                right++;
-            }
-            count += right - (mid + 1);
+           
+            v.insert(lower_bound(v.begin(), v.end(), 2LL * a[i]), 2LL * a[i]);
         }
 
-        vector<int> temp;
-
-        int i = low;
-        int j = mid + 1;
-
-        while (i <= mid && j <= high) {
-            if (nums[i] <= nums[j]) {
-                temp.push_back(nums[i]);
-                i++;
-            } else {
-                temp.push_back(nums[j]);
-                j++;
-            }
-        }
-
-        while (i <= mid) {
-            temp.push_back(nums[i]);
-            i++;
-        }
-
-        while (j <= high) {
-            temp.push_back(nums[j]);
-            j++;
-        }
-
-        for (int k = low; k <= high; k++) {
-            nums[k] = temp[k - low];
-        }
-
-        return count;
-    }
-
-    int mergeSort(vector<int>& nums, int low, int high) {
-
-        if (low >= high)
-            return 0;
-
-        int mid = low + (high - low) / 2;
-
-        int count = 0;
-
-        count += mergeSort(nums, low, mid);
-        count += mergeSort(nums, mid + 1, high);
-        count += merge(nums, low, mid, high);
-
-        return count;
-    }
-
-    int reversePairs(vector<int>& nums) {
-
-        return mergeSort(nums, 0, nums.size() - 1);
+        return ans; 
     }
 };
